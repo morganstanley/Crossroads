dfdaf11a23cb661ffb8ad9f6a534b849f2d0fa1d
KojoBoat <egyirchristian07@gmail.com>
2022-09-15 16:09:22 +0000

    crossroads-partition success update

--- src/Crossroads/Services/InspectService.cs
+++ src/Crossroads/Services/InspectService.cs
@@ -35,7 +35,7 @@ namespace Crossroads.Services
                 ProcessStartInfo startInfo = new ProcessStartInfo
                 {
                     FileName = packagePath,
-                    Arguments = "LauncherInspect"
+                    Arguments = "inspect"
                 };
                 result = await processService.GetConsoleOutputAsync(startInfo, 3000);
             }
