18a313d5cbc884da4adfd4a57a42b6fe5cacb4aa
jyffarh <joycelokko1@gmail.com>
2022-10-18 07:48:16 +0000

    add apphost to launcher

--- src/Crossroads.Launcher/Crossroads.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-cmd /c "dotnet build -r linux-x64 Crossroads.Launcher.csproj"
-cmd /c "dotnet build -r win-x64 Crossroads.Launcher.csproj"
-cmd /c "dotnet build ../Crossroads/Crossroads.csproj"
-EXIT
