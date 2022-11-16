af3601301f9bda9cdb3bca9b4299e19f5f84a7fa
jyffarh <joycelokko1@gmail.com>
2022-11-16 08:48:23 +0000

    remove unused dependency

--- src/Crossroads/Services/AppHostService.cs
+++ src/Crossroads/Services/AppHostService.cs
@@ -18,7 +18,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace Crossroads.Services
