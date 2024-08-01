db702f7b26df923e30b29a10d2c97e5ce63bb06c
Mimi <mimi.flynn@morganstanley.com>
2024-08-01 15:39:35 -0400

    update documentation engine

--- site/src/pages/404.js
+++ site/src/pages/404.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Hero from '../components/hero';
 import Layout from '../components/layout';
-import Seo from '../components/seo';
 
 const NotFoundPage = ({ data, location }) => {
   return (
     <Layout data={data} location={location}>
-      <div className="home-main">
-        <article className="hero">
-          <div className="pane">
-            <header className="content">
-              <h2>404 Not Found</h2>
-            </header>
-          </div>
-        </article>
+      <div className="main home-main">
+        <Hero title="404 Not Found">
+          What you are looking for is either no longer here or has moved.
+        </Hero>
       </div>
-      <Seo title="404: Not Found" />
     </Layout>
   );
 };
 
 export default NotFoundPage;
+
+export const Head = () => <title>404: Not Found</title>;
 
 export const pageQuery = graphql`
   query {
